d8f49aa333629bd86a58fa8afe09fd92b9a9260a
DixonCh <dixonchaudhary7@gmail.com>
2019-12-17 16:58:49 +0545

    Add files via upload

---
+++ Area_of_Rectangle/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Area_of_Rectangle
+{
+    class Rectangle
+    {
+        //member variables
+        double length;
+        double width;
+        public void AcceptDetails()
+        {
+            length = 9.5;
+            width = 6.5;
+        }
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+
+        public void Display() {
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("The Length of Rectangle is: {0}", length);
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("The Width of Rectangle is: {0}", width);
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("The Area of Rectangle is: {0}", GetArea());
+            Console.WriteLine("-------------------------------");
+        }
+    }
+    class ExecuteRectangle {
+        static void Main(string[] args) {
+            Rectangle r = new Rectangle();
+            r.AcceptDetails();
+            r.Display();
+            Console.ReadLine();
+        }
+    }
+}
